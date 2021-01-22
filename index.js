01190aa35986836a1a1142243de128ac554b5587
Subham Mitra <subham.mitra21@gmail.com>
2021-01-22 14:23:29 +0000

    indentation and spacing improved

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 const readLineSync = require('readline-sync');
 
-const user_response = parseInt(readLineSync.question('Welcome to Unit Converter. Which conversion would you like to use?\n1. Meter to Kilometer\n2. Foot to Centimeter\n'));
+const userResponse = parseInt(readLineSync.question('Welcome to Unit Converter. Which conversion would you like to use?\n1. Meter to Kilometer\n2. Foot to Centimeter\n'));
 
-console.log(`You selected ${user_response}`);
-logic(user_response);
+console.log(`You selected ${userResponse}`);
 
  function meterToKilometer(meter)
 {
-  return meter/1000;
+   return meter/1000;
 }
 
 function footToCentimeter(foot)
 {
-  return foot*30.48;
+   return foot*30.48;
 }
 
-function logic(user_response)
+switch(userResponse)
 {
-  if(user_response===1)
-  {
-    const meter =parseInt(readLineSync.question('Please enter your input in meters\n'));
-    console.log(meterToKilometer(meter));
-  }
+   case 1:
+   const meter = parseInt(readLineSync.question('Please enter your input in meters\n'));
+   console.log(meterToKilometer(meter));
+   break;
  
- else if(user_response===2)
-  {
-    const foot =parseInt(readLineSync.question('Please enter your input in foot\n'));
-    console.log(footToCentimeter(foot));
-  }
+   case 2:
+   const foot = parseInt(readLineSync.question('Please enter your input in foot\n'));
+   console.log(footToCentimeter(foot));
+   break;
 
-  else
-  console.log('Invalid Option');
+   default:
+   console.log('Invalid Option');
+   break;
 }
